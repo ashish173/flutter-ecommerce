e93bd519f3c9b2c30d751797057c009ddf0e91f6
Ashish Singh <aitashish173@gmail.com>
2018-12-27 12:13:05 +0530

    adds card on button press with state

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('hey I am here!'),
-        ),
-        body: Column(children: <Widget>[
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('hey I am here!'),
+      ),
+      body: Column(
+        children: <Widget>[
           Container(
             margin: EdgeInsets.all(10.0),
             child: RaisedButton(
-              onPressed: () {},
-              child: Text("Add product")
-            ),
+              onPressed: () {
+                setState(() {
+                  _products.add("Advanced food tester");                                  
+                });
+              }, 
+              child: Text("Add product")),
           ),
-          Card(
+          Column(children: _products.map((element) => Card(
             child: Column(
               children: <Widget>[
                 Image.asset('assets/profile.jpeg'),
-                Text('Food Paradise')
+                Text(element)
               ],
             ),
-          ),
-        ]),
-      )
-    );
+          ),).toList(),)
+      ]),
+    ));
   }
 }
