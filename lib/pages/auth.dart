57159175b19be8f0fe6bc2549a73e63a48732475
Ashish Singh <aitashish173@gmail.com>
2019-01-04 06:46:18 +0530

    refactor auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -16,6 +16,42 @@ class _AuthPageState extends State<AuthPage> {
   String _passwordValue;
   bool _acceptTerms = false;
 
+  DecorationImage _buildBackgroundImage() {
+    return DecorationImage(
+              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+              image: AssetImage('assets/background.jpg'),
+              fit: BoxFit.cover,
+            );
+  }
+
+  Widget _buildEmailTextField(){
+    return TextField(
+                    decoration: InputDecoration(labelText: 'E-mail'),
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (String value) {
+                      setState(() {
+                        _emailValue = value;
+                      });
+                    },
+                  );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Password'),
+      keyboardType: TextInputType.text,
+      onChanged: (String value) {
+        setState(() {
+          _passwordValue = value;
+        });
+      },
+    );
+  }
+
+  void _submitForm() {
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,40 +60,18 @@ class _AuthPageState extends State<AuthPage> {
         ),
         body: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
-              image: AssetImage('assets/background.jpg'),
-              fit: BoxFit.cover,
-            ),
+            image: _buildBackgroundImage(),
           ),
           padding: EdgeInsets.all(10.0),
           child: Center(
             child: SingleChildScrollView(
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'E-mail'),
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (String value) {
-                      setState(() {
-                        _emailValue = value;
-                      });
-                    },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    keyboardType: TextInputType.text,
-                    onChanged: (String value) {
-                      setState(() {
-                        _passwordValue = value;
-                      });
-                    },
-                  ),
+                  _buildEmailTextField(),
+                  _buildPasswordTextField(),
                   RaisedButton(
                     child: Text('LOGIN'),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    },
+                    onPressed: _submitForm,
                   ),
                 ],
               ),  
